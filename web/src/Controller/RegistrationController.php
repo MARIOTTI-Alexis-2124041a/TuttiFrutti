d401152470cf7b8393d7cb890669f5919b44a163
alexis <alexis.mariotti@laposte.net>
2024-02-16 09:38:25 +0100

    Implemented turbo for login and tried for register

--- web/src/Controller/RegistrationController.php
+++ web/src/Controller/RegistrationController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\UX\Turbo\TurboBundle;
 
 class RegistrationController extends AbstractController
 {
@@ -41,6 +42,11 @@ class RegistrationController extends AbstractController
 
             return $this->redirectToRoute('app_login');
         }
+        /*
+        if (TurboBundle::STREAM_FORMAT === $request->getPreferredFormat()) {
+            // If the request comes from Turbo, set the content type as text/vnd.turbo-stream.html and only send the HTML to update
+            $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+        }*/
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
